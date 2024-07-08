54568c1fdd8f99193452873bd023239e4548cc78
HuyKhoiVN <khoitrinh1@outlook.com>
2024-07-08 13:06:32 +0700

    khoi - add category, bill, color, size controller

---
+++ AppAPI/Controllers/ImageController.cs
@@ -0,0 +1,65 @@
+﻿using AppAPI.Service;
+using AppData.Context;
+using AppData.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+
+namespace AppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : Controller
+    {
+        CRUDApi<Image> _crud;
+        TStoreDb _context = new TStoreDb();
+
+        public ImageController()    
+        {
+            CRUDApi<Image> crud = new CRUDApi<Image>(_context, _context.Images);
+            _crud = crud;
+        }
+
+        [HttpGet("get-all-image")]
+        public IEnumerable<Image> GetImages()
+        {
+            return _crud.GetAllItems().ToList();
+        }
+
+        
+        [HttpPost("create-new-image")]
+        public bool CreateImage(string imageUrl, Guid idproductdetail)
+        {
+            Image image = new Image();
+            image.Id = Guid.NewGuid();
+            image.ImageUrl = imageUrl;
+            image.Status = true;
+            image.IdPorductDetail = idproductdetail;
+            return _crud.CreateItem(image);
+        }
+ 
+        [HttpPut("edit-image")]
+        public bool UpdateImage(Guid id, string imageUrl, bool status, Guid idproductdetail)
+        {
+            var image = _context.Images.FirstOrDefault(x => x.Id == id);
+            if (image != null)
+            {
+                image.ImageUrl = imageUrl;
+                image.Status = status;
+                image.IdPorductDetail = idproductdetail;
+                return _crud.UpdateItem(image);
+            }
+            return false;
+        }
+
+        [HttpDelete("delete-image")]
+        public bool DeleteImage(Guid id)
+        {
+            var image = _context.Images.FirstOrDefault(y => y.Id == id);
+            if (image != null)
+            {
+                return _crud.DeleteItem(image);
+            }
+            return false;
+        }
+    }
+}
--- AppData/obj/Debug/net8.0/AppData.AssemblyInfo.cs
+++ AppData/obj/Debug/net8.0/AppData.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+348644b3ebe3c827de6f9570fa1467b0151a465d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e86d8117f3d693f50f1a481fc35913aaae6e76e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
